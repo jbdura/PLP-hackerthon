78517a735c655a87897e088b1619b9e746f0bdd6
jbdura <bidurajohnson@gmail.com>
2024-09-25 11:31:36 +0300

    Cleaned the needed details

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import DeployButton from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
@@ -34,17 +33,21 @@ export default function RootLayout({
         >
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
+              
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
+                    <Link href={"/"}>Home</Link>
                     <div className="flex items-center gap-2">
-                      <DeployButton />
+
                     </div>
                   </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+
+                  {/* {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />} */}
+
                 </div>
               </nav>
+              
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
               </div>

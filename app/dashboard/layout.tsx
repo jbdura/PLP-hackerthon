dab2078d3b210e614df6d508696083215bda6021
jbdura <bidurajohnson@gmail.com>
2024-09-29 12:26:07 +0300

    Cleaned up dashboard code

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -23,14 +23,6 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        // <html lang="en" className={GeistSans.className} suppressHydrationWarning>
-        //     <body className="bg-background text-foreground">
-        //         <ThemeProvider
-        //             attribute="class"
-        //             defaultTheme="system"
-        //             enableSystem
-        //             disableTransitionOnChange
-        //         >
         <>
             <div className="bg-background text-foreground">
 
@@ -62,20 +54,7 @@ export default function RootLayout({
                                 {children}
                             </div>
 
-                            {/* <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                                <p>
-                                    Powered by{" "}
-                                    <a
-                                        href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                                        target="_blank"
-                                        className="font-bold hover:underline"
-                                        rel="noreferrer"
-                                    >
-                                        Supabase
-                                    </a>
-                                </p>
-                                // <ThemeSwitcher />
-                            </footer> */}
+                            
                         </div>
                     </main>
                 </ThemeProvider >
@@ -83,8 +62,6 @@ export default function RootLayout({
 
         </>
 
-        //         </ThemeProvider >
-        //     </body>
-        // </html>
+        
     );
 }
